5f7714e6560d38d0cc81c68b6c7224fa04689584
Suraj <Surajippili@gmail.com>
2023-01-22 19:24:07 +0530

    Added DbContext

---
+++ NZWalks.API/NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,15 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+	public class Walk
+	{
+		public Guid Id { get; set; }		
+		public string Name { get; set; }	
+		public double Length { get; set; }
+		public Guid RegiondId { get; set; }
+		public Guid WalkDiffcultyId { get; set; }
+
+		// NavigationProperty
+		public Region Region { get; set; }
+		public WalkDiffculty WalkDiffculty { get; set; }
+	}
+}
